270cfaafc192fb117f3e1e8d31a7c6f4d2e66849
Henry Nguyen <nguyen.henry796@gmail.com>
2023-04-06 00:57:03 -0400

    Did exercise

---
+++ FactoryPattern/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Motorcycle : IVehicle
+    {
+        public Motorcycle()
+        {
+        }
+
+        public void Build()
+        {
+            Console.WriteLine("The Motorcycle is revving up!!!");
+        }
+    }
+}
